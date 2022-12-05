e222a1d7dcca26340c20f3c879f6b8d62b1cfae2
Sam Yuan <yy19902439@126.com>
2022-12-05 22:23:36 +0800

    impl: add parameter for different branch running
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- dist/index.js
+++ dist/index.js
@@ -2881,26 +2881,39 @@ const cli = __nccwpck_require__(283);
 
 // most @actions toolkit packages have async methods
 async function run() {
-  //  const nameToGreet = core.getInput('who-to-greet');
+  const runningBranch = core.getInput('runningBranch');
   try {
-    core.info(`Start to build env`);
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    core.info(`Linux header`);
-    let linuxheader_0 = cli.ConvertCommand("sudo apt-get install -y linux-headers-5.15.0-1023-azure");
-    cli.executeCommand(linuxheader_0)
-    
-    core.info(`BCC deb`);
-    let BCC_0 = cli.ConvertCommand("wget https://github.com/sustainable-computing-io/kepler-ci-artifacts/releases/download/v0.25.0/bcc_v0.25.0.tar.gz");
-    cli.executeCommand(BCC_0)
-    let BCC_1 = cli.ConvertCommand("tar -zxvf bcc_v0.25.0.tar.gz");
-    cli.executeCommand(BCC_1)
-    let BCC_2 = cli.ConvertCommand("sudo dpkg -i libbcc_0.25.0-1_amd64.deb");
-    cli.executeCommand(BCC_2)
-
-    core.info(`kubectl`);
-    let kubectl_0 = cli.ConvertCommand("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl");
-    cli.executeCommand(kubectl_0)
-
+    if (runningBranch == 'bcc') {
+      core.info(`Start to build env`);
+      core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+      core.info(`Linux header`);
+      let linuxheader_0 = cli.ConvertCommand("sudo apt-get install -y linux-headers-5.15.0-1023-azure");
+      cli.executeCommand(linuxheader_0)
+      
+      core.info(`BCC deb`);
+      let BCC_0 = cli.ConvertCommand("wget https://github.com/sustainable-computing-io/kepler-ci-artifacts/releases/download/v0.25.0/bcc_v0.25.0.tar.gz");
+      cli.executeCommand(BCC_0)
+      let BCC_1 = cli.ConvertCommand("tar -zxvf bcc_v0.25.0.tar.gz");
+      cli.executeCommand(BCC_1)
+      let BCC_2 = cli.ConvertCommand("sudo dpkg -i libbcc_0.25.0-1_amd64.deb");
+      cli.executeCommand(BCC_2)
+      return
+    }
+    if (runningBranch == 'kind') {
+      core.info(`kubectl`);
+      let kubectl_0 = cli.ConvertCommand("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl");
+      cli.executeCommand(kubectl_0)
+      core.info(`to do for kind`);
+      return
+    }
+    if (runningBranch == 'microshift') {
+      core.info(`kubectl`);
+      let kubectl_0 = cli.ConvertCommand("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl");
+      cli.executeCommand(kubectl_0)
+      core.info(`to do for microshift`);
+      return
+    }
+    core.error('runningBranch should in value of [bcc, kind, microshift]')
   } catch (error) {
     core.setFailed(error.message);
   }
