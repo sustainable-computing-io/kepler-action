59847ea321efe5c8b73655a6cff22b18ce9221f0
Sam Yuan <yy19902439@126.com>
2023-01-07 19:45:50 +0800

    dev: update use shelljs and clone github action to avoid file not found
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const core = require('@actions/core');
-const cli = require('./command');
+const shell = require('shelljs');
 
 async function bcc() {
   core.info(`Start to build env`);
   core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
   core.info(`Linux header`);
-  let linuxheader_0 = cli.ConvertCommand("sudo apt-get install -y linux-headers-5.15.0-1023-azure");
-  cli.executeCommand(linuxheader_0)
-      
+  if (shell.exec("sudo apt-get install -y linux-headers-5.15.0-1023-azure").code !== 0){
+    shell.echo("fail to install linux headers");
+    shell.exit(1);
+  }
   core.info(`BCC deb`);
-  let BCC_0 = cli.ConvertCommand("wget https://github.com/sustainable-computing-io/kepler-ci-artifacts/releases/download/v0.25.0/bcc_v0.25.0.tar.gz");
-  cli.executeCommand(BCC_0)
-  let BCC_1 = cli.ConvertCommand("tar -zxvf bcc_v0.25.0.tar.gz");
-  cli.executeCommand(BCC_1)
-  let BCC_2 = cli.ConvertCommand("sudo dpkg -i libbcc_0.25.0-1_amd64.deb");
-  cli.executeCommand(BCC_2)
+  if (shell.exec("wget https://github.com/sustainable-computing-io/kepler-ci-artifacts/releases/download/v0.25.0/bcc_v0.25.0.tar.gz").code !==0){
+    shell.echo("fail to get BCC deb");
+    shell.exit(1);
+  }
+  if (shell.exec("tar -zxvf bcc_v0.25.0.tar.gz").code !== 0) {
+    shell.echo("fail to untar BCC deb");
+    shell.exit(1);
+  }
+  if (shell.exec("sudo dpkg -i libbcc_0.25.0-1_amd64.deb").code !== 0) {
+    shell.echo("fail to install BCC deb");
+    shell.exit(1);
+  }
   return
 }
 
-async function kind() {
+async function kubectl() {
   core.info(`kubectl`);
-  let kubectl_0 = cli.ConvertCommand("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl");
-  cli.executeCommand(kubectl_0)
-  //core.info(`to do for kind`);
+  if (shell.exec("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl").code !== 0) {
+    shell.echo("fail to install kubectl");
+    shell.exit(1);
+  }
+}
+
+async function kind() {
   core.info(`kind`);
-  let kubectl_1 = cli.ConvertCommand("bash -c ./kind/common.sh");
-  cli.executeCommand(kubectl_1)
+  shell.exec("git clone https://github.com/sustainable-computing-io/KeplerK8SAction --depth=1");
+  shell.exec("bash -c ./KeplerK8SAction/kind/common.sh")
   return
 }
 
@@ -38,12 +49,10 @@ async function run() {
     }
     if (runningBranch == 'kind') {
       bcc()
+      kubectl()
       return kind()
     }
     if (runningBranch == 'microshift') {
-      core.info(`kubectl`);
-      let kubectl_0 = cli.ConvertCommand("curl -LO https://dl.k8s.io/release/v1.25.4/bin/linux/amd64/kubectl");
-      cli.executeCommand(kubectl_0)
       core.info(`to do for microshift`);
       return
     }
